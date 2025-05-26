bdfc8cf24820aa0f60d01fc48598d44b066f4d14
shimyoungmin <ym.sim@cardoc.kr>
2025-05-27 00:13:33 +0900

    new commit

---
+++ YoungminFramework/YoungminFramework.xcframework/ios-arm64_x86_64-simulator/YoungminFramework.framework/Headers/YoungminFramework.h
@@ -0,0 +1,18 @@
+//
+//  YoungminFramework.h
+//  YoungminFramework
+//
+//  Created by 심영민 on 5/26/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for YoungminFramework.
+FOUNDATION_EXPORT double YoungminFrameworkVersionNumber;
+
+//! Project version string for YoungminFramework.
+FOUNDATION_EXPORT const unsigned char YoungminFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YoungminFramework/PublicHeader.h>
+
+
